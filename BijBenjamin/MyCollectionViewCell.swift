64f93928d8ea08d3038372dfef651eab0f2ebaac
Vedad Piric <vedadpiric@MacBook-Pro-van-Vedad.local>
2017-06-26 22:50:27 +0200

    first commit

---
+++ BijBenjamin/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  BijBenjamin
+//
+//  Created by Vedad Piric on 10-05-17.
+//  Copyright © 2017 Vedad Piric. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ BijBenjamin/Order.swift
@@ -0,0 +1,9 @@
+//
+//  Order.swift
+//  BijBenjamin
+//
+//  Created by Vedad Piric on 13-05-17.
+//  Copyright © 2017 Vedad Piric. All rights reserved.
+//
+
+import Foundation
